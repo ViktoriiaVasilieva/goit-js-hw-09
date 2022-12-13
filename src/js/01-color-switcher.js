239b0de4167346dde209ac8d21c049990d9a920c
vesnika <vasiliyeva93@gmail.com>
2022-12-13 21:50:11 +0200

    first task

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+let interval = null;
+refs.startBtn.addEventListener('click', getRandomHexColor);
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(interval);
+  refs.startBtn.disabled = false;
+});
+
+function getRandomHexColor() {
+  interval = setInterval(() => {
+    return (document.getElementsByTagName(
+      'body'
+    )[0].style.backgroundColor = `#${Math.floor(
+      Math.random() * 16777215
+    ).toString(16)}`);
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
