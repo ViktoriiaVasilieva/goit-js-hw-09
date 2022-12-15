f5168714015708e39c82672f08112024b7ab4f29
vesnika <vasiliyeva93@gmail.com>
2022-12-15 13:46:24 +0200

    changes

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
+import Notiflix from 'notiflix';
+
+refs = {
+  btnSubmit: document.querySelector('button'),
+  input: document.querySelectorAll('input'),
+};
+
+refs.btnSubmit.addEventListenet('submit', createPromise);
+// refs.input('input');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Fulfill(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Reject(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
